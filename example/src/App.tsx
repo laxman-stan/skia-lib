30437310b021dbed567ca558f5c1add6c2eff8b4
laxman-stan <laxmikant@getstan.app>
2023-09-23 01:13:29 +0530

    chore: release

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,21 @@
+/* eslint-disable react-native/no-inline-styles */
+import { Canvas } from '@shopify/react-native-skia';
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
+// import { SkImage } from 'react-native-skia-lib';
 
 export default function App() {
-  const [result] = React.useState<number | undefined>();
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Basic app</Text>
+      <Text>abcd</Text>
+      <Canvas style={{ flex: 1, backgroundColor: 'green' }}>
+        {/* <SkImage
+          rect={rect(0, 0, 100, 100)}
+          image="https://plus.unsplash.com/premium_photo-1695370767476-d942b94f027c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        /> */}
+      </Canvas>
     </View>
   );
 }
