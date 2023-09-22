7bfca5f84aadacf27d69a4ebc4969d07d0bb3a96
laxman-stan <laxmikant@getstan.app>
2023-09-23 00:34:19 +0530

    chore: release 0.0.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,42 @@
+import {
+  Blur,
+  Image,
+  type ImageProps,
+  type RectProps,
+  type SkRect,
+  type SkiaProps,
+  useImage,
+} from '@shopify/react-native-skia';
+import React from 'react';
+import { type ReactNode } from 'react';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
+}
+
+type Props = SkiaProps<RectProps> &
+  Omit<SkiaProps<ImageProps>, 'image'> & {
+    image: string;
+    rect: SkRect;
+    loader?: ReactNode;
+    blur?: number;
+  };
+
+export function SkImage({
+  loader,
+  image,
+  blur,
+  fit = 'cover',
+  ...rest
+}: Props) {
+  const imageSrc = useImage(image);
+  if (!image && loader) {
+    return <>{loader}</>;
+  }
+
+  return (
+    <Image {...rest} fit={fit} image={imageSrc}>
+      {blur && <Blur blur={blur} />}
+    </Image>
+  );
 }
